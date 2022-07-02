b2a4ca55c1fd96a797971e9e78bc9f7836281cb2
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-02 15:55:33 -0400

    added hero image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
 
 const Home = () => {
     return ( 
-    <div name='home'>
-        <div>
-            <div>
-                <h2>
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+        <div className='max-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                     I'm a Full Stack Developer
                 </h2>
-                <p>
+                <p className='text-gray-500 py-4>
                     My name is Emmanuel, I love coding. 
                 </p>
 
@@ -22,6 +22,10 @@ const Home = () => {
                         </span>
                     </button>
                 </div>
+            </div>
+
+            <div>
+                <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full' />
             </div>
         </div>
     </div>
