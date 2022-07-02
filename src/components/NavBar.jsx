8fdb7e583e40755743564d4c3712ea56650df1da
Emmanuel6093 <elorenzo6093@gmail.com>
2022-07-02 14:09:24 -0400

    adding hamburger icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const NavBar = () => {
+
+    const [nav, setNav] = useState(false); 
 
     const links = [
         {
@@ -36,9 +38,15 @@ const NavBar = () => {
            {links.map(({id, link}) => (
              <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
            ))}
+         </ul>
 
+         <div  onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500'>
+            {nav ? <FaTimes size={30} /> :<FaBars size={30} /> }
+         </div>
 
-        </ul>
+         <ul className='flex flex-col justify-center '>
+            <li>home</li>
+         </ul>
     </div>
 
     );
